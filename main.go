715753651b032f835dc29864629b75518ae38a9b
fsorodrigues <fsorodrigues@gmail.com>
2025-10-13 18:12:26 +1300

    feat: add logging to file

--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
+	"github.com/charmbracelet/log"
 )
 
+func getLogger(filename string) (*log.Logger, error) {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o666)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating the logger: %s", err)
+	}
+
+	logger := log.NewWithOptions(file, log.Options{
+		ReportCaller:    true,
+		ReportTimestamp: true,
+		TimeFormat:      time.Kitchen,
+		Level:           log.DebugLevel,
+		Prefix:          "[dbt_lsp]",
+	})
+
+	return logger, nil
+}
+
 func main() {
-	fmt.Printf("hello from lsp")
+	logger, err := getLogger("/home/felipperodrigues/downloads/log.txt")
+	if err != nil {
+		logger.Error(err)
+		panic(err)
+	}
+	logger.Info("dbt LSP started")
 }
