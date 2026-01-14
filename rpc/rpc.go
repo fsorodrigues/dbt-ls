57f02939ddca284e6069011bdee8126025af8916
fsorodrigues <fsorodrigues@gmail.com>
2026-01-14 14:17:22 +1300

    feat: rpc package

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -9,7 +9,9 @@ import (
 )
 
 type JsonRpcMsg struct {
-	Method string `json:"method"`
+	JsonRpc string `json:"jsonrpc"`
+	Id      int    `json:"id"`
+	Method  string `json:"method"`
 }
 
 func EncodeMsg(msg any) (string, error) {
@@ -21,26 +23,51 @@ func EncodeMsg(msg any) (string, error) {
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content), nil
 }
 
-func DecodeMsg(msg []byte) (string, int, error) {
+func DecodeMsg(msg []byte) (string, []byte, error) {
 	header, content, msgFound := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
 	if !msgFound {
-		return "", 0, errors.New("Did not find separator \\r\\n\\r\\n while parsing message")
+		return "", nil, errors.New("Did not find separator \\r\\n\\r\\n while parsing message")
 	}
 
 	_, contentLengthBytes, lengthFound := bytes.Cut(header, []byte{':', ' '})
 	if !lengthFound {
-		return "", 0, errors.New("Did not find content length while parsing message header")
+		return "", nil, errors.New("Did not find content length while parsing message header")
 	}
 
 	contentLength, err := strconv.Atoi(string(contentLengthBytes))
 	if err != nil {
-		return "", 0, errors.New("Could not parse the length of message value in message header")
+		return "", nil, errors.New("Could not parse the length of message value in message header")
 	}
 
 	var baseMsg JsonRpcMsg
 	if err := json.Unmarshal(content[:contentLength], &baseMsg); err != nil {
-		return "", 0, errors.New("Error unmarshalling json data")
+		return "", nil, errors.New("Error unmarshalling json data")
 	}
 
-	return baseMsg.Method, contentLength, nil
+	return baseMsg.Method, content[:contentLength], nil
+}
+
+func Split(data []byte, _ bool) (advance int, token []byte, err error) {
+	header, content, streamFound := bytes.Cut(data, []byte{'\r', '\n', '\r', '\n'})
+	if !streamFound {
+		return 0, nil, nil
+	}
+
+	_, contentLengthBytes, lengthFound := bytes.Cut(header, []byte{':', ' '})
+	if !lengthFound {
+		return 0, nil, errors.New("Did not find content length header while parsing stdin stream")
+	}
+
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, nil, errors.New("Could not parse the length of stdin stream in stream header")
+	}
+
+	if len(content) < contentLength {
+		return 0, nil, nil
+	}
+
+	totalLength := len(header) + 4 + contentLength
+
+	return totalLength, data[:totalLength], nil
 }
