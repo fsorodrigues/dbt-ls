cc2bad57ce81e98547419711b4aa800137fdd774
fsorodrigues <fsorodrigues@gmail.com>
2026-03-27 17:38:37 +1300

    feat: add window/showMessage LSP notification type
    Add ShowMessageParams, ShowMessageNotification, and MessageType* constants
    to support the window/showMessage LSP notification method.

---
+++ lsp/window.go
@@ -0,0 +1,18 @@
+package lsp
+
+const (
+	MessageTypeError   = 1
+	MessageTypeWarning = 2
+	MessageTypeInfo    = 3
+	MessageTypeLog     = 4
+)
+
+type ShowMessageParams struct {
+	Type    int    `json:"type"`
+	Message string `json:"message"`
+}
+
+type ShowMessageNotification struct {
+	Notification
+	Params ShowMessageParams `json:"params"`
+}
