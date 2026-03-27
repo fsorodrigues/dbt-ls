87183663a55fb0cdac9671d87ec85f19f96482e6
fsorodrigues <fsorodrigues@gmail.com>
2026-03-27 17:30:21 +1300

    refactor: extract dbt config types into analysis/config.go

---
+++ analysis/config.go
@@ -0,0 +1,35 @@
+package analysis
+
+type DbtConfig struct {
+	Name    string
+	Sources map[string]*DbtConfigSource
+}
+
+type DbtConfigSource struct {
+	Name     string
+	Database string
+	Schema   string
+	Tables   map[string]*DbtTable
+}
+
+type DbtTable struct {
+	Name       string `yaml:"name"`
+	Identifier string `yaml:"identifier"`
+	SourceFile string
+}
+
+type DbtSource struct {
+	Name     string      `yaml:"name"`
+	Database string      `yaml:"database"`
+	Schema   string      `yaml:"schema"`
+	Tables   []*DbtTable `yaml:"tables"`
+}
+
+type DbtSources struct {
+	Sources []*DbtSource `yaml:"sources"`
+}
+
+type DbtProject struct {
+	Name string `yaml:"name"`
+	DbtSources
+}
